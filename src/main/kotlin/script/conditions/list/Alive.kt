b47085851842992175d7711a68b7065749b43efb
4o4E <869951226@qq.com>
2022-05-04 10:19:36 +0800

    添加是否死亡判断

---
+++ src/main/kotlin/script/conditions/list/Alive.kt
@@ -0,0 +1,11 @@
+package top.e404.escript.script.conditions.list
+
+import top.e404.escript.annotation.ConditionSign
+import top.e404.escript.script.conditions.Condition
+import top.e404.escript.script.conditions.ConditionParser
+
+@ConditionSign
+object Alive : ConditionParser {
+    override val headRegex = Regex("(?i)isAlive|isNotDead")
+    override fun parse(content: Any?) = Condition { !it.isDead }
+}
---
+++ src/main/kotlin/script/conditions/list/Dead.kt
@@ -0,0 +1,11 @@
+package top.e404.escript.script.conditions.list
+
+import top.e404.escript.annotation.ConditionSign
+import top.e404.escript.script.conditions.Condition
+import top.e404.escript.script.conditions.ConditionParser
+
+@ConditionSign
+object Dead : ConditionParser {
+    override val headRegex = Regex("(?i)isDead")
+    override fun parse(content: Any?) = Condition { it.isDead }
+}
