8829d0fd044989da699bfdea68614cc678c5827d
4o4E <869951226@qq.com>
2022-06-06 01:19:56 +0800

    修改reload流程

--- src/main/kotlin/command/Reload.kt
+++ src/main/kotlin/command/Reload.kt
@@ -1,6 +1,7 @@
 package top.e404.escript.command
 
 import org.bukkit.command.CommandSender
+import top.e404.escript.config.CommandTrigger
 import top.e404.escript.config.Config
 import top.e404.escript.config.CustomCooldown
 import top.e404.escript.config.WorldScript
@@ -16,11 +17,12 @@ object Reload : AbstractCommand(
 ) {
     override val help = "&a/escript reload &f重载".color()
     override fun onCommand(sender: CommandSender, args: Array<out String>) {
-        Config.load(sender)
-        ScriptManager.load(sender)
-        CustomCooldown.load(sender)
-        WorldScript.load(sender)
-        HookManager.update()
+        Config.load(sender) // 配置文件
+        ScriptManager.load(sender) // 脚本管理器
+        CommandTrigger.load(sender) // 指令触发器
+        CustomCooldown.load(sender) // 自定义cd
+        WorldScript.load(sender) // 世界脚本
+        HookManager.update() // 更新挂钩
         sender.sendMsgWithPrefix("&a重载完成")
     }
 }
